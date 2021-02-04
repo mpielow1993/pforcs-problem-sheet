f2210969c5ebc56b7e3b0b38226eec79dde6f0d5
mpielow1993 <michaelpielow@hotmail.com>
2021-02-04 23:19:32 +0000

    edit to divide by height in m squared

--- week2/bmi.py
+++ week2/bmi.py
@@ -1,13 +1,24 @@
+import math
+
+def validate_positive(measurement):
+    if measurement <= 0:
+        raise ValueError
+
 def calculate_bmi():
     try:
-        weight_in_kg = input('Enter weight in kilograms:   ')
-        weight_in_kg = int(weight_in_kg)
-        height_in_cm = input('Enter height in centimetres:   ')
-        height_in_cm = int(height_in_cm)
-        height_in_m = height_in_cm / 100
-        bmi = weight_in_kg / height_in_m
-        print('BMI is ' + str(bmi))
-    except(TypeError, ValueError):
-        print('Invalid value entered')
+        weight_in_kg = float(input('Enter weight in kilograms: '))
+        validate_positive(weight_in_kg)
+        height_in_cm = float(input('Enter height in centimetres: '))
+        validate_positive(height_in_cm)
+        bmi = weight_in_kg / math.pow((height_in_cm / 100), 2)
+        print('BMI is ' + str(round(bmi, 2)))
+    except(ValueError):
+        print('Invalid value entered. You must enter a number greater than 0')
+    finally:
+        choice = input('Enter any key to try again, or enter "exit" (case-sensitive) to quit')
+        if choice == 'exit':
+            print('Goodbye')
+        else:
+           calculate_bmi() 
 
 calculate_bmi()
