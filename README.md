55d1ff8d22ecd54a2069d6c99054543c08f2870f
mpielow1993 <michaelpielow@hotmail.com>
2021-02-13 20:16:14 +0000

    create helpers, create collatz function, edit bmi function, remove ipynb files, add empty  __init__ files, create main.py, update README

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # pforcs-problem-sheet
 Programming for Cybersecurity repository (GMIT)
+
+Notes:
+
+* Each function can be tested conviniently by running main.py, located in the root directory of this repository.
+* Within this file, the following format can be observed:
+
+    - # Week <Week No>
+    - # <Function for that week>
+
+* Simply uncomment the line for the function and run locally
+  
---
+++ week2/week2_helpers.py
@@ -0,0 +1,36 @@
+# Signature:   parse_float()
+# Params:   input
+# Description:   Returns true if input can be parsed to a float, false otherwise
+def parse_float(input):
+    try:
+        float(input)
+        return True
+    except ValueError:
+        return False
+
+
+# Method: in_float_range()
+# Params: input, min, max
+# Description:   Checks that input, min and max are of type float. Returns true if input is in-between min & max, false otherwise
+def in_float_range(input, min, max):
+
+    param_dict = {
+        'Input': input,
+        'Minimum': min,
+        'Maximum': max
+    }
+
+    for key, value in param_dict.items():
+        if not parse_float(value):
+            print(f'\nERROR: {key} cannot be converted to a decimal')
+            return False
+        
+    if float(input) <= min:
+        print(f'\nERROR: Input cannot be less than or equal to {min}')
+        return False
+
+    if float(input) > max:
+        print(f'\nERROR: Input cannot be greater than {max}')
+        return False
+
+    return True
